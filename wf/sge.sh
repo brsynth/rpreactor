079925b9b680492be1b2aa4f73f7721cd7638a42
Thomas Duigou <thomas.duigou@inra.fr>
2019-08-01 18:45:16 +0200

    Update SGE cluster script for consistency

--- wf/sge.sh
+++ wf/sge.sh
@@ -1,5 +1,6 @@
 #$ -S /bin/bash
 
+export PATH=$HOME/miniconda3/bin:$PATH
 unset PYTHONPATH
-source $HOME/miniconda3/bin/activate $HOME/.conda/envs/pyrule
+source $HOME/miniconda3/bin/activate $HOME/miniconda3/envs/mcts
 {exec_job}
