2bdec63a7e0521c036f817f5c9784e67b50d657b
Baudoin <baudoin.delepine@imean-biotech.com>
2020-09-16 18:22:07 +0200

    feat: fusion project rpchemtools
    brsynth/rpchemtools: Minimalist toolbox to deal with chemicals.
    https://github.com/brsynth/rpchemtools
    
    Basically, rpchemtools contains tricks to standardize chemicals with
    RDKit for our usage in a biological context.
    
    The functions from Sequences.py have been put in Standardizer.py.

--- rpreactor/Utils.py
+++ rpreactor/Utils.py
@@ -196,3 +196,40 @@ def handle_results(list_list_rdmol):
             logging.warning("Cannot handle a tuple of result, skipped")
             logging.warning("{}".format(e))
     return list_list_inchikeys, list_list_inchis, list_list_smiles  # Quick but dirty
+
+
+def convert_depiction(idepic, itype='smiles', otype={'inchikey'}):
+    """Convert chemical depiction to others type of depictions
+
+    :param  idepic: string depiction to be converted, str
+    :param   itype: type of depiction provided as input, str
+    :param   otype: types of depiction to be generated, {"", "", ..}
+    :return odepic: generated depictions, {"otype1": "odepic1", ..}
+
+    Usage example:
+    - convert_depiction(idepic='CCO', otype={'inchi', 'smiles', 'inchikey'})
+    - convert_depiction(idepic='InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3', itype='inchi', otype={'inchi', 'smiles', 'inchikey'})
+    """
+    # Import (if needed)
+    if itype == 'smiles':
+        rdmol = Chem.MolFromSmiles(idepic, sanitize=True)
+    elif itype == 'inchi':
+        rdmol = Chem.MolFromInchi(idepic, sanitize=True)
+    else:
+        raise NotImplementedError('"{}" is not a valid input type'.format(itype))
+    if rdmol is None:  # Check imprt
+        raise Exception('Import error from depiction "{}" of type "{}"'.format(idepic, itype))
+
+    # Export
+    odepic = dict()
+    for item in otype:
+        if item == 'smiles':
+            odepic[item] = Chem.MolToSmiles(rdmol)  # MolToSmiles is tricky, one mays want to check the possible options..
+        elif item == 'inchi':
+            odepic[item] = Chem.MolToInchi(rdmol)
+        elif item == 'inchikey':
+            odepic[item] = MolToInchiKey(rdmol)
+        else:
+            raise NotImplementedError('"{}" is not a valid output type'.format(otype))
+
+    return odepic
---
+++ rpreactor/chemical/standardizer.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+"""
+Everything to standardize chemicals (metabolites).
+
+The idea is to use a Standardizer object to store a set of "standardization rules", and to (re)use this object
+to standardize each chemical. We call "filters" those "standardization rules" to avoid confusing them with
+reaction rules. Each filter is applied sequentially. For convenience, some pre-defined sequences of filters
+are defined in the Standardizer class.
+
+@author: Baudoin Delepine, 2016-2017
+@author: Thomas Duigou, 2018-2019
+"""
+
+from rpchemtools import Sequences
+from rpchemtools.Filters import Filters
+from rdkit.Chem import Cleanup, SanitizeMol, SanitizeFlags
+from rdkit.Chem.AllChem import AssignStereochemistry
+
+
+class Standardizer(object):
+    """Handle standardization of compound(s) through user-defined "filters"."""
+
+    def __call__(self, mol):
+        """Calling a Standardizer object like a function is the same as calling its "compute" method.
+
+        From:
+            https://github.com/mcs07/MolVS/blob/master/molvs/standardize.py
+        """
+        return self.compute(mol)
+
+    def __init__(self, sequence_fun=None, params=None):
+        """Set up parameters for the standardization."""
+        # Function to be used for standardizing compounds
+        # Add you own function as method class
+        if sequence_fun is None:
+            self._sequence_fun = Standardizer.sequence_minimal
+        elif callable(sequence_fun):     # guess: fun_filters is the function itself
+            self._sequence_fun = sequence_fun
+        elif type(sequence_fun) == str:  # guess: sequence_fun is the name of the function
+            self._sequence_fun = getattr(Sequences, sequence_fun)
+        # Arguments to be passed to any custom standardization function
+        self._params = params if params else None
+
+    @staticmethod
+    def sequence_minimal(mol):
+        """Minimal standardization."""
+        SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        AssignStereochemistry(mol, cleanIt=True, force=True, flagPossibleStereoCenters=True)  # Fix bug TD201904.01
+        return mol
+
+    @staticmethod
+    def sequence_rr_legacy(mol):
+        """Sequence of filters applied for the first version of RetroRules."""
+        F = Filters()
+        Cleanup(mol)
+        SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        AssignStereochemistry(mol, cleanIt=True, force=True, flagPossibleStereoCenters=True)  # Fix bug TD201904.01
+        mol = F.remove_isotope(mol)
+        mol = F.neutralise_charge(mol)
+        SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        mol = F.keep_biggest(mol)
+        mol = F.add_hydrogen(mol, addCoords=True)
+        mol = F.kekulize(mol)
+        return mol
+
+    @staticmethod
+    def sequence_tunable(
+            mol,
+            OP_REMOVE_ISOTOPE=True, OP_NEUTRALISE_CHARGE=True,
+            OP_REMOVE_STEREO=False, OP_COMMUTE_INCHI=False,
+            OP_KEEP_BIGGEST=True, OP_ADD_HYDROGEN=True,
+            OP_KEKULIZE=True, OP_NEUTRALISE_CHARGE_LATE=True
+    ):
+        """Tunable sequence of filters for standardization.
+
+        Operations will made in the following order:
+         1 RDKit Cleanup      -- always
+         2 RDKIT SanitizeMol  -- always
+         3 Remove isotope     -- optional (default: True)
+         4 Neutralise charges -- optional (default: True)
+         5 RDKit SanitizeMol  -- if 4 or 5
+         6 Remove stereo      -- optional (default: False)
+         7 Commute Inchi      -- if 6 or optional (default: False)
+         8 Keep biggest       -- optional (default: True)
+         9 RDKit SanitizeMol  -- if any (6, 7, 8)
+        10 Add hydrogens      -- optional (default: True)
+        11 Kekulize           -- optional (default: True)
+        """
+        F = Filters()
+        # Always perform the basics..
+        Cleanup(mol)
+        SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        AssignStereochemistry(mol, cleanIt=True, force=True, flagPossibleStereoCenters=True)  # Fix bug TD201904.01
+        #
+        if OP_REMOVE_ISOTOPE:
+            mol = F.remove_isotope(mol)
+        if OP_NEUTRALISE_CHARGE:
+            mol = F.neutralise_charge(mol)
+        if any([OP_REMOVE_ISOTOPE, OP_REMOVE_ISOTOPE]):
+            SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        #
+        if OP_REMOVE_STEREO:
+            mol = F.remove_stereo(mol)
+            OP_COMMUTE_INCHI = True
+        if OP_COMMUTE_INCHI:
+            mol = F.commute_inchi(mol)
+        if OP_KEEP_BIGGEST:
+            mol = F.keep_biggest(mol)
+        if any([OP_REMOVE_STEREO, OP_COMMUTE_INCHI, OP_KEEP_BIGGEST]):
+            SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        #
+        if OP_NEUTRALISE_CHARGE_LATE:
+            mol = F.neutralise_charge(mol)
+            SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_ALL, catchErrors=False)
+        #
+        if OP_ADD_HYDROGEN:
+            mol = F.add_hydrogen(mol, addCoords=True)
+        if OP_KEKULIZE:
+            mol = F.kekulize(mol)
+        #
+        return mol
+
+    def compute(self, mol):
+        """Standardize the provided RDKit molecule."""
+        if self._params is None:
+            return self._sequence_fun(mol)
+        else:
+            return self._sequence_fun(mol, **self._params)
