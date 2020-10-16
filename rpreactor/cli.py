ee8e9a850cacfb816ec7fc6ed10e210a27d58788
Baudoin <baudoin.delepine@imean-biotech.com>
2020-10-16 15:39:18 +0200

    doc: cli help message use the right program name

--- rpreactor/cli.py
+++ rpreactor/cli.py
@@ -64,7 +64,7 @@ def __cli():
         r.compute(timeout=args.fire_timeout)
         r.write_json()
 
-    parser = argparse.ArgumentParser(description=help)
+    parser = argparse.ArgumentParser(description=help, prog='python -m rpreactor.cli')
     parser.add_argument('--fire_timeout', help='Rule firing timeout (seconds). Default: 60.', default=60, type=int)
     parser.add_argument('--ofile', help='Output file to store results. Default to STDOUT if none provided')
     parser.add_argument('--compress', action='store_true', help='Enable gzip compression (only when output to file).')
