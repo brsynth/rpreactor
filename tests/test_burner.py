2214d761091a3b4e808351ad3e479b126aed1d32
Thomas Duigou <thomas.duigou@inrae.fr>
2020-12-17 11:37:42 +0100

    style: fix typos

--- tests/test_burner.py
+++ tests/test_burner.py
@@ -9,7 +9,7 @@ from rpreactor.rule.burner import RuleBurner
 
 
 # Data for tests
-substate_inchi = 'InChI=1S/C3H6O3/c1-2(4)3(5)6/h2,4H,1H3,(H,5,6)'
+substrate_inchi = 'InChI=1S/C3H6O3/c1-2(4)3(5)6/h2,4H,1H3,(H,5,6)'
 reaction_smarts = '([#8&v2:1](-[#6&v4:2](-[#6&v4:3](-[#8&v2:4]-[#1&v1:5])=[#8&v2:6])(-[#6&v4:7](-[#1&v1:8])(-[#1&v1:9])-[#1&v1:10])-[#1&v1:11])-[#1&v1:12])>>([#15&v5](=[#8&v2])(-[#8&v2]-[#1&v1])(-[#8&v2]-[#1&v1])-[#8&v2:1]-[#6&v4:2](-[#6&v4:3](-[#8&v2:4]-[#1&v1:5])=[#8&v2:6])(-[#6&v4:7](-[#1&v1:8])(-[#1&v1:9])-[#1&v1:10])-[#1&v1:11].[#7&v3](=[#6&v4]1:[#7&v3]:[#6&v4](-[#8&v2]-[#1&v1]):[#6&v4]2:[#7&v3]:[#6&v4](-[#1&v1]):[#7&v3](-[#6&v4]3(-[#1&v1])-[#8&v2]-[#6&v4](-[#6&v4](-[#8&v2]-[#15&v5](=[#8&v2])(-[#8&v2]-[#1&v1])-[#8&v2]-[#15&v5](-[#8&v2]-[#1&v1:12])(=[#8&v2])-[#8&v2]-[#1&v1])(-[#1&v1])-[#1&v1])(-[#1&v1])-[#6&v4](-[#8&v2]-[#1&v1])(-[#1&v1])-[#6&v4]-3(-[#8&v2]-[#1&v1])-[#1&v1]):[#6&v4]:2:[#7&v3]:1-[#1&v1])-[#1&v1])'
 tuple_product_inchikeys = ('CSZRNWHGZPKNKY-UHFFFAOYSA-N', 'QGWNDRXFNXRZMB-UHFFFAOYSA-N')
 tuple_product_smiles = ('[H][O][C](=[O])[C]([H])([O][P](=[O])([O][H])[O][H])[C]([H])([H])[H]', '[H][N]=[c]1[n][c]([O][H])[c]2[n][c]([H])[n]([C]3([H])[O][C]([H])([C]([H])([H])[O][P](=[O])([O][H])[O][P](=[O])([O][H])[O][H])[C]([H])([O][H])[C]3([H])[O][H])[c]2[n]1[H]')
@@ -125,14 +125,14 @@ def test_handle_result():
 @pytest.mark.parametrize(
     "smarts,inchi,expected_inchi",
     [
-        (reaction_smarts, substate_inchi, ['InChI=1S/C3H7O6P/c1-2(3(4)5)9-10(6,7)8/h2H,1H3,(H,4,5)(H2,6,7,8)', 'InChI=1S/C10H15N5O11P2/c11-10-13-7-4(8(18)14-10)12-2-15(7)9-6(17)5(16)3(25-9)1-24-28(22,23)26-27(19,20)21/h2-3,5-6,9,16-17H,1H2,(H,22,23)(H2,19,20,21)(H3,11,13,14,18)'])
+        (reaction_smarts, substrate_inchi, ['InChI=1S/C3H7O6P/c1-2(3(4)5)9-10(6,7)8/h2H,1H3,(H,4,5)(H2,6,7,8)', 'InChI=1S/C10H15N5O11P2/c11-10-13-7-4(8(18)14-10)12-2-15(7)9-6(17)5(16)3(25-9)1-24-28(22,23)26-27(19,20)21/h2-3,5-6,9,16-17H,1H2,(H,22,23)(H2,19,20,21)(H3,11,13,14,18)'])
     ]
 )
 def test_compute(smarts, inchi, expected_inchi):
     # Things that are expected to return results
     rb = RuleBurner(with_hs=True)
     rb.insert_rsmarts([reaction_smarts])
-    rb.insert_inchi([substate_inchi])
+    rb.insert_inchi([substrate_inchi])
     rb.compute(rule_mol='*')
     result = [x for x in rb.compute(rule_mol='*')][0]
     assert all(x in result for x in ['rule_id', 'substrate_id', 'product_list', 'product_inchikeys', 'product_inchis', 'product_smiles']), result
@@ -144,7 +144,7 @@ def test_compute_wrong_reaction(caplog):
     with caplog.at_level(logging.ERROR):
         rb = RuleBurner()
         rb.insert_rsmarts(['DUMMY'])
-        rb.insert_inchi([substate_inchi])
+        rb.insert_inchi([substrate_inchi])
         result = [x for x in rb.compute(rule_mol='*')]
     assert "RULE-CONVERSION-ERROR" in caplog.text, (result, caplog.text)
 
