f478eecf70e0a200b88251e26c9cc7a0f088afe2
Thomas Duigou <thomas.duigou@inrae.fr>
2020-12-18 11:42:54 +0100

    refactor(cli): refactor cli methods

--- rpreactor/cli.py
+++ rpreactor/cli.py
@@ -14,80 +14,88 @@ import argparse
 from rpreactor.rule.burner import RuleBurner
 
 
-def export_json(results, ofile, compress, indent=True):
+def jsonify(results, indent=True):
     """Export results as JSON.
 
-    :param  results:  results from  RuleBurner.compute()
-    :param  ofile:    Path to the output JSON file. If 'None', will display to STDOUT.
-    :param  compress: Compress the file if True.
-    :param  indent:   Indent the JSON for humans if True.
+    :param results: results from  RuleBurner.compute()
+    :type results: list of dict
+    :param indent: indent the JSON for humans if True.
+    :type indent: bool
+    :returns json_str: JSON string representation
+    :rtype: str
     """
-    json_str = json.dumps(results, indent=indent)
-    pdir = os.path.abspath(os.path.dirname(ofile))
-    os.makedirs(pdir, exist_ok=True)
-    ofile = os.path.abspath(ofile)
-    # Handling file handler
+    # Trim-out the RDKit objects under the "product_list" key
+    results_no_mol = [{k: v for k, v in d.items() if k != 'product_list'} for d in results]
+    return json.dumps(results_no_mol, indent=indent)
+
+
+def write(text, ofile=None, compress=False):
+    """Write text to STDOUT or file.
+
+    :param text: character string to be written
+    :type text: str
+    :param ofile:
+    :type ofile: str
+    :param compress:
+    :type compress: bool
+    """
     if ofile:
+        ofile = os.path.abspath(ofile)
+        pdir = os.path.abspath(os.path.dirname(ofile))
+        os.makedirs(pdir, exist_ok=True)
         if compress:
             ofh = gzip.open(ofile, 'wb', compresslevel=9)
+            ofh.write(text.encode())
         else:
             ofh = open(ofile, 'w')
+            ofh.write(text)
     else:
         ofh = sys.stdout
-    # Handling compression
-    if ofile and compress:
-        ofh.write(json_str.encode())
-    else:
-        ofh.write(json_str)
+        ofh.write(text)
     ofh.close()
 
 
-def __cli():
-    """Command line interface."""
+def __inline_mode(args):
+    """Execution mode to be used when a single rule and a single chemical
+    are provided through CLI.
+    """
+    r = RuleBurner(with_hs=args.with_hs, with_stereo=args.with_stereo)
+    if args.rid:
+        r.insert_rsmarts({args.rid: args.rsmarts})
+    else:
+        r.insert_rsmarts([args.rsmarts])
+    if args.cid:
+        r.insert_inchi({args.cid: args.inchi})
+    else:
+        r.insert_inchi([args.inchi])
+    return [x for x in r.compute(rule_mol='*', timeout=args.fire_timeout)]
 
-    help = "Apply rules on chemicals."
 
-    def inline_mode(args):
-        """Execution mode to be used when a single rule and a single chemical
-        are provided through CLI.
-        """
-        r = RuleBurner(rsmarts_list=[args.rsmarts], inchi_list=[args.inchi], rid_list=[args.rid], cid_list=[args.cid],
-                       with_hs=args.with_hs, with_stereo=args.with_stereo)
-        results = [x for x in r.compute(timeout=args.fire_timeout)]
-        export_json(results, ofile=args.ofile, compress=args.compress)
+def __infile_mode(args):
+    """
+    Execution mode to be used when rules and chemicals are provided
+    in CSV files.
+    """
+    import csv
 
-    def infile_mode(args):
-        """
-        Execution mode to be used when rules and chemicals are provided
-        in CSV files.
-        """
+    with open(args.rfile, 'r') as ifh:
+        reader = csv.DictReader(ifh, delimiter='\t')
+        rsmarts = {row['rule_id']: row['rule_smarts'] for row in reader}
 
-        rsmarts_list = list()
-        rids_list = list()
+    with open(args.cfile, 'r') as ifh:
+        reader = csv.DictReader(ifh, delimiter='\t')
+        inchis = {row['chem_id']: row['inchi'] for row in reader}
 
-        inchi_list = list()
-        cids_list = list()
+    r = RuleBurner(with_hs=args.with_hs, with_stereo=args.with_stereo)
+    r.insert_rsmarts(rsmarts)
+    r.insert_inchi(inchis)
+    return [x for x in r.compute(rule_mol='*', timeout=args.fire_timeout)]
 
-        import csv
 
-        with open(args.rfile, 'r') as ifh:
-            reader = csv.DictReader(ifh, delimiter='\t')
-            for row in reader:
-                rsmarts_list.append(row['rule_smarts'].strip())
-                rids_list.append(row['rule_id'].strip())
+def __build_arg_parser(prog='python -m rpreactor.cli'):
+    desc = "Apply rules on chemicals."
 
-        with open(args.cfile, 'r') as ifh:
-            reader = csv.DictReader(ifh, delimiter='\t')
-            for row in reader:
-                inchi_list.append(row['inchi'].strip())
-                cids_list.append(row['chem_id'].strip())
-
-        r = RuleBurner(rsmarts_list=rsmarts_list, inchi_list=inchi_list, rid_list=rids_list, cid_list=cids_list,
-                       with_hs=args.with_hs, with_stereo=args.with_stereo)
-        results = [x for x in r.compute(timeout=args.fire_timeout)]
-        export_json(results, ofile=args.ofile, compress=args.compress)
-
-    parser = argparse.ArgumentParser(description=help, prog='python -m rpreactor.cli')
+    parser = argparse.ArgumentParser(description=desc, prog=prog)
     parser.add_argument('--fire_timeout', help='Rule firing timeout (seconds). Default: 60.', default=60, type=int)
     parser.add_argument('--ofile', help='Output file to store results. Default to STDOUT if none provided')
     parser.add_argument('--compress', action='store_true', help='Enable gzip compression (only when output to file).')
@@ -100,7 +108,7 @@ def __cli():
     subparsers = parser.add_subparsers(help='Input mode')
 
     parser_inline = subparsers.add_parser('inline', help='Get inputs from command line')
-    parser_inline.set_defaults(func=inline_mode)
+    parser_inline.set_defaults(func=__inline_mode)
     parser_inline.add_argument('--rsmarts', help='Reaction rule SMARTS', required=True)
     # parser_inline.add_argument('--csmiles', help='Chemical SMILES depiction', required=True)
     parser_inline.add_argument('--inchi', help='Chemical inchi depiction', required=True)
@@ -108,14 +116,13 @@ def __cli():
     parser_inline.add_argument('--cid', help='Chemical ID, optional')
 
     parser_file = subparsers.add_parser('infile', help='Get inputs from files')
-    parser_file.set_defaults(func=infile_mode)
+    parser_file.set_defaults(func=__infile_mode)
     parser_file.add_argument(
             '--rfile', required=True, help=' '.join([
                     'Reaction rule file.',
                     'Tab separated columns.',
                     'One reaction rule per line.',
-                    'Mandatory column: rule_smarts.',
-                    'Optional column: rule_id.',
+                    'Mandatory column: rule_smarts, rule_id.',
                     'Other columns will be ignored.'
                     ])
             )
@@ -124,23 +131,26 @@ def __cli():
                     'Chemical file.',
                     'Tab separated columns.',
                     'One chemical per line.',
-                    'Mandatory column: inchi.',
-                    'Optional column: chem_id.',
+                    'Mandatory column: inchi, chem_id.',
                     'Other columns will be ignored.'
                     ])
             )
 
-    # Logging
+    return parser
+
+
+def __cli():
     logging.basicConfig(
             stream=sys.stderr, level=logging.INFO,
             datefmt='%d/%m/%Y %H:%M:%S',
             format='%(asctime)s -- %(levelname)s -- %(message)s'
             )
-
-    # Execute right mode
+    parser = __build_arg_parser()
     args = parser.parse_args()
     try:
-        args.func(args)
+        results = args.func(args)
+        results_json = jsonify(results)
+        write(results_json, ofile=args.ofile, compress=args.compress)
     except AttributeError:
         parser.print_help()
         sys.exit(1)
