12dad489b2e1f070ac2cbdb3bb80f70b0b2b5d3a
Baudoin <baudoin.delepine@imean-biotech.com>
2021-05-06 14:55:28 +0200

    feat: silence RDKit warnings

--- rpreactor/__init__.py
+++ rpreactor/__init__.py
@@ -5,3 +5,10 @@ del get_versions
 # Expose main classes for end-user convenience
 from rpreactor.rule.burner import RuleBurner
 from rpreactor.rule.utils import create_db_from_retrorules
+
+
+# Silence most of RDKit complaints
+from rdkit import RDLogger
+
+RD_LOGGER = RDLogger.logger()
+RD_LOGGER.setLevel(RDLogger.CRITICAL)
